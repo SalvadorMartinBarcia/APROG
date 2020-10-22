9c7ac4656ab3054538f5463fe2e3d204ebafb9d6
Salvador Martin <48014156+SalvadorMartinBarcia@users.noreply.github.com>
2020-10-22 16:46:31 +0200

    Simplificación de facturas

--- practica_1b/p1b/src/ApartadoC/ApartadoC.hs
+++ practica_1b/p1b/src/ApartadoC/ApartadoC.hs
@@ -18,7 +18,6 @@ module ApartadoC.ApartadoC where
 
     -- precio de una factura
     precioFactura :: Factura -> Float
-	precioFactura fact = []
     precioFactura fact = precioFacturaux (ventas fact)
         where   precioFacturaux [] = 0
                 precioFacturaux (x:xs) = (precioVenta x) + (precioFacturaux xs)
@@ -100,23 +99,25 @@ module ApartadoC.ApartadoC where
                                                 [v]
                                             else []
     
-	
-	instance Eq Venta where
-		x == y = (nid (articulo x)) == (nid (articulo y))
-		x /= y = not (x == y)
-	-- eliminar repeticiones de artículos en una factura
-	elim3 :: Factura -> Factura
-	elim3 fact = Factura (elim3aux [] (ventas fact))
-		where 	elim3aux seen [] = seen
-				elim3aux seen (x:xs) 	| elem x seen = elim3aux (unirAux seen seen x) xs
-										| otherwise = elim3aux (seen ++ [x]) xs
-				unirAux (y:ys) seen x = if (x /= y) then
-											unirAux ys seen x
-										else
-											(ventas (elim1 (Factura seen) (articulo x))) ++ (Venta (articulo x) ((cantidad x)+(cantidad y))
-	
-    mainB :: IO ()
-    mainB = do
+
+    instance Eq Venta where
+        x == y = (nid (articulo x)) == (nid (articulo y))
+        x /= y = not (x == y)
+        
+    -- eliminar repeticiones de artículos en una factura
+    elim3 :: Factura -> Factura
+    elim3 fact = Factura (elim3aux [] (ventas fact))
+        where   elim3aux seen [] = seen
+                elim3aux seen (x:xs)    | elem x seen = elim3aux (unirAux seen seen x) xs
+                                        | otherwise = elim3aux (seen ++ [x]) xs
+                unirAux (y:ys) seen x = if (x /= y) then
+                                            unirAux ys seen x
+                                        else
+                                            (ventas (elim1 (Factura seen) (articulo x))) ++ [(Venta (articulo x) ((cantidadVenta x)+(cantidadVenta y)))]
+                                            
+
+    mainC :: IO ()
+    mainC = do
 
         let a1 = Articulo 1 "Coco" 1.05
         let v1 = Venta a1 3
@@ -159,22 +160,22 @@ module ApartadoC.ApartadoC where
         print("TEST DE ELIMINACIONES")
         print("Eliminacion en una factura de las ventas relativas a un determinado articulo: "++show (elim1 fact3 a1))
         print("Eliminacion en una factura de las ventas de aquellas relativas a una cantidad menor que una determinada: "++show (elim2 fact3 2))
-		print("LOLOLOLO: "++show (elim3 fact3))
+        print("Simplificacion de facturas: "++show (elim3 fact3))
 		
-		-- "venta1 test precio = 3.1499999"
-		-- "factura1 test precio = 8.15"
-		-- "fusion 2 facturas: {venta \"coco\" 3, {ventaunitaria \"chocolate\", {venta \"platano\" 2, {venta \"napolitana\" 1, venta \"coco\" 3}"
-		-- "precio total de un articulo en una factura: 6.2999997"
-		-- ""
-		-- "TEST DE CONVERSION A CADENAS"
-		-- "conversion a cadena de articulo: \"coco\""
-		-- "conversion a cadena de venta: venta \"coco\" 3"
-		-- "conversion a cadena de factura: {venta \"coco\" 3, {ventaunitaria \"chocolate\", venta \"platano\" 2}"
-		-- ""
-		-- "TEST DE BUSQUEDAS"
-		-- "busqueda en una factura de las ventas relativas a un articulo: {venta \"coco\" 3, venta \"coco\" 3}"
-		-- "busqueda en una factura de las ventas relativas a una lista de articulos: {ventaunitaria \"chocolate\", venta \"napolitana\" 1}"
-		-- ""
-		-- "TEST DE ELIMINACIONES"
-		-- "eliminacion en una factura de las ventas relativas a un determinado articulo: {ventaunitaria \"chocolate\", {venta \"platano\" 2, venta \"napolitana\" 1}"
-		-- "eliminacion en una factura de las ventas de aquellas relativas a una cantidad menor que una determinada: {Venta \"Coco\" 3, {Venta \"Platano\" 2, Venta \"Coco\" 3}"
+        -- "venta1 test precio = 3.1499999"
+        -- "factura1 test precio = 8.15"
+        -- "fusion 2 facturas: {venta \"coco\" 3, {ventaunitaria \"chocolate\", {venta \"platano\" 2, {venta \"napolitana\" 1, venta \"coco\" 3}"
+        -- "precio total de un articulo en una factura: 6.2999997"
+        -- ""
+        -- "TEST DE CONVERSION A CADENAS"
+        -- "conversion a cadena de articulo: \"coco\""
+        -- "conversion a cadena de venta: venta \"coco\" 3"
+        -- "conversion a cadena de factura: {venta \"coco\" 3, {ventaunitaria \"chocolate\", venta \"platano\" 2}"
+        -- ""
+        -- "TEST DE BUSQUEDAS"
+        -- "busqueda en una factura de las ventas relativas a un articulo: {venta \"coco\" 3, venta \"coco\" 3}"
+        -- "busqueda en una factura de las ventas relativas a una lista de articulos: {ventaunitaria \"chocolate\", venta \"napolitana\" 1}"
+        -- ""
+        -- "TEST DE ELIMINACIONES"
+        -- "eliminacion en una factura de las ventas relativas a un determinado articulo: {ventaunitaria \"chocolate\", {venta \"platano\" 2, venta \"napolitana\" 1}"
+        -- "eliminacion en una factura de las ventas de aquellas relativas a una cantidad menor que una determinada: {Venta \"Coco\" 3, {Venta \"Platano\" 2, Venta \"Coco\" 3}"
